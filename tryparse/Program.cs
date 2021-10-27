80aa5626f25ffddc615a23aae351c5bdccbd8912
Ronja Zakrisson <zakrissonronja@gmail.com>
2021-10-27 10:23:36 +0200

    innjn

---
+++ tryparse/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+/*string n = "3";
+
+int m = 4;
+
+int r = n + m;
+
+int y;
+if (n=="3")
+{
+    y=3;
+}
+
+Console.WriteLine(r);
+
+Console.ReadLine();*/
+int age = 0;
+bool success = false;
+
+while (success != true)
+{
+System.Console.WriteLine("Your age:");
+string input = Console.ReadLine();
+
+//int age = int.Parse(input);
+
+success = int.TryParse(input, out age);
+}
+
+if (success == true)
+{
+   if (age < 18)
+{
+    System.Console.WriteLine("Du får inte ta körkort");
+}
+else
+{
+    System.Console.WriteLine("Du får ta körkort");
+}
+
+Console.ReadLine(); 
+}
+
